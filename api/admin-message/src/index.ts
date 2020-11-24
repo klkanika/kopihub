7a3b083bd4cd2081bb6fe9a3d37721e98d13fdf5
krittiyach <krittiya.ch@yipintsoi.com>
2020-11-24 15:02:15 +0700

    [admin-message] added index.ts

---
+++ api/admin-message/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+
+const { PORT } = process.env
+const app = express();
+
+// define a route handler for the default home page
+// app.get( "/", ( req, res ) => {
+//   res.send( "Hello world!" );
+// });
+
+app.post("/api/admin-message" , async (req,res) => {
+    console.log('call api/admin-message')
+    console.log('req', req)
+    res.send("ok")
+} );
+
+// start the Express server
+app.listen( PORT || 3030, () => {
+    console.log( `server started at http://localhost:${ PORT || 3030 }` );
+} );
+
+
