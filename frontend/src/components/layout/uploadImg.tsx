59d145858c30bcb1c70e0c83c66976f28bcd9185
Anupat <scheduleAPITime>
2020-12-11 01:38:27 +0700

    fix api

--- frontend/src/components/layout/uploadImg.tsx
+++ frontend/src/components/layout/uploadImg.tsx
@@ -9,7 +9,7 @@ export const uploadImg = async (options: any) => {
     fmData.append("myFile", file);
     try {
         const res = await axios.post(
-            "https://kopihub-frontend-ura2vr67wa-as.a.run.app/uploadfile",
+            "https://kopihub-api-ura2vr67wa-as.a.run.app/uploadfile",
             // "https://a3bd0e2b3833.ap.ngrok.io/uploadfile",
             fmData,
             config
